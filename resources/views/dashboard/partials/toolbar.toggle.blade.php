6d56349b527324c875420b5df60af650d7ec33bb
José Expósito Ávila <EXAVJO@magtel.es>
2023-03-30 18:31:51 +0200

    añadiendo toolbar.togle.blade

---
+++ resources/views/dashboard/partials/toolbar.toggle.blade.php
@@ -0,0 +1,22 @@
+<div class="toolbar-overlay">
+
+    <div class="toolbar-inner">
+
+        <div class="toolbar-title">
+
+            <ul class="toolbar-items">
+
+                <li class="toolbar-item"></li>
+                <li class="toolbar-item"></li>
+                <li class="toolbar-item"></li>
+                <li class="toolbar-item"></li>
+                <li class="toolbar-item"></li>
+                <li class="toolbar-item"></li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
