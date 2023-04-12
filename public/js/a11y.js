4977ef52cec9925c8b3d85a45455eea747ba4fc8
José Expósito Ávila <EXAVJO@magtel.es>
2023-04-12 18:03:15 +0200

    ajustando restore toolbar

--- public/js/a11y.js
+++ public/js/a11y.js
@@ -4,27 +4,77 @@ $(() => {
         var p_estile = getComputedStyle(p_element);
         var p_color = p_estile.color;*/
 
-        /* var p_color = getComputedStyle(document.querySelector('p')).color;
+        var p_color = getComputedStyle(document.querySelector('p')).color;
         var span_color = getComputedStyle(document.querySelector('span')).color;
-        var h1_color = getComputedStyle(document.querySelector('.h1')).color;
-        var i_color = getComputedStyle(document.querySelector('i')).color;
+        /* var h1_color = getComputedStyle(document.querySelector('.h1')).color; */
+        /* var i_color = getComputedStyle(document.querySelector('i')).color;  */
         var div_color = getComputedStyle(document.querySelector('div')).color;
         var span_color = getComputedStyle(document.querySelector('span')).color;
-        var button_color = getComputedStyle(document.querySelector('button')).color;
+        /* var button_color = getComputedStyle(document.querySelector('button')).color; */
         var toolbar_text_color = getComputedStyle(document.querySelector('.toolbar-text')).color;
         var toolbar_inner_color = getComputedStyle(document.querySelector('.toolbar-inner')).color;
+        var toolbar_icon_color = getComputedStyle(document.querySelector('.toolbar-icon')).color;   
 
+        if($('.toolbar_icon')[0]){
+            var header_bc = getComputedStyle(document.querySelector('header')).backgroundColor;
+        } 
+
+        if($('.sidebar-button')[0]){
+            var sidebar_button_bc = getComputedStyle(document.querySelector('.sidebar-button')).backgroundColor;
+        }
+
+        if($('.dashboard')[0]){
+            var dashboard_c = getComputedStyle(document.querySelector('.dashboard')).color;
+        }
+
+        if($('.bx bx-menu sidebarBtn')[0]){
+            var sidebarBtn_c = getComputedStyle(document.querySelector('.bx bx-menu sidebarBtn'));
+        }
+
+        if($('i')[0]){
+            var i_c = getComputedStyle(document.querySelector('i')).color;
+        } 
+
+        if($('#spaceTopMenu')){
+            var spaceTopMenu_bc = getComputedStyle(document.querySelector('#spaceTopMenu')).backgroundColor;
+        }
+
+        if($('header')[0]){
+            var header_bc = getComputedStyle(document.querySelector('header')).backgroundColor;
+            $('header').css('background', header_bc);
+        }
+
+        if($('nav')[0]){
+            var nav_bc = getComputedStyle(document.querySelector('nav')).backgroundColor;
+            $('nav').css('background', nav_bc);
+        }
+
+        if($('.sidebar')[0]){
+            var sidebar_bc = getComputedStyle(document.querySelector('.sidebar')).backgroundColor;
+        }
+
+        if($('.sidebar li')[0]){
+            var sidebar_li_bc = getComputedStyle(document.querySelector('.sidebar li')).backgroundColor; 
+        }
+
+        if($('.sidebar li')[0]){
+            var sidebar_li_bc = getComputedStyle(document.querySelector('.sidebar li')).backgroundColor; 
+        }
+
+        if($('.logo-details')[0]){
+            var logo_details_bc = getComputedStyle(document.querySelector('.logo-details')).backgroundColor; 
+        }
         
-        var sidebar_bc = getComputedStyle(document.querySelector('.sidebar')).backgroundColor;
+        /* var sidebar_bc = getComputedStyle(document.querySelector('.sidebar')).backgroundColor;
         var sidebar_li_bc = getComputedStyle(document.querySelector('.sidebar li')).backgroundColor;     
-        var nav_bc = getComputedStyle(document.querySelector('nav')).backgroundColor;
-        var home_section_bc = getComputedStyle(document.querySelector('.home-section')).backgroundColor;
+        var nav_bc = getComputedStyle(document.querySelector('nav')).backgroundColor; */
+        /* var home_section_bc = getComputedStyle(document.querySelector('.home-section')).backgroundColor; */
         var toolbar_inner_bc = getComputedStyle(document.querySelector('.toolbar-inner')).backgroundColor;
-        var spaceTopMenu_bc = getComputedStyle(document.querySelector('#spaceTopMenu')).backgroundColor;
+        /* var spaceTopMenu_bc = getComputedStyle(document.querySelector('#spaceTopMenu')).backgroundColor; */
         var div_bc = getComputedStyle(document.querySelector('div')).backgroundColor;
-        var button_bc = getComputedStyle(document.querySelector('button')).backgroundColor; 
-        var mainData_bc = getComputedStyle(document.querySelector('.mainData')).backgroundColor;
-        var row_bc = getComputedStyle(document.querySelector('.row')).backgroundColor;  */
+        /* var button_bc = getComputedStyle(document.querySelector('button')).backgroundColor; */
+        /* var mainData_bc = getComputedStyle(document.querySelector('.mainData')).backgroundColor;
+        var row_bc = getComputedStyle(document.querySelector('.row')).backgroundColor; */
 
         function defaultColors(){
 
@@ -33,16 +83,58 @@ $(() => {
             $('.toolbar-text').css('color', toolbar_text_color);
             $('p').css('color', p_color);
             $('div').css('color', div_color);
-            $('i').css('color', i_color);
+            $('.toolbar-icon').css('color', toolbar_icon_color); 
 
-            $('.sidebar').css('background', sidebar_bc);
-            $('.sidebar li').css('background', sidebar_li_bc);
-            $('nav').css('background', nav_bc);
-            $('.home-section').css('background', home_section_bc);
+            if($('i')[0]){
+                $('i').css('color', i_c);
+            } 
+
+            if($('.sidebar')[0]){
+                $('.sidebar').css('background', sidebar_bc);
+            }
+
+            if($('#spaceTopMenu')[0]){
+                $('#spaceTopMenu').css('background', spaceTopMenu_bc);
+            }
+
+            if($('.logo-details')[0]){
+                $('.logo-details').css('background', logo_details_bc);
+            }
+
+            if($('.sidebar-button')[0]){
+                $('.sidebar-button').css('background', sidebar_button_bc);
+            }
+
+            if($('.dashboard')[0]){
+                $('.dashboard').css('color', dashboard_c);
+            }
+
+            if($('.toolbar_icon')[0]){
+                $('.toolbar-icon').css('background', toolbar_icon_bc); 
+            }      
+    
+            if($('header')[0]){
+                $('header').css('background', header_bc);
+            }
+    
+            if($('nav')[0]){
+                $('nav').css('background', nav_bc);
+            }
+    
+            if($('.sidebar')[0]){
+                $('.sidebar').css('background', sidebar_bc);
+            }
+    
+            if($('.sidebar li')[0]){
+                $('.sidebar li').css('background', sidebar_bc);
+            }
+
+            /* $('nav').css('background', nav_bc); */
+            /* $('.home-section').css('background', home_section_bc); */
             $('.toolbar-inner').css('background', toolbar_inner_bc);
-            $('#spaceTopMenu').css('background', spaceTopMenu_bc);
-            $('div').css('background', div_bc);
-            $('.mainData').css('background', mainData_bc);
+            /* $('#spaceTopMenu').css('background', spaceTopMenu_bc); */
+            /* $('div').css('background', div_bc); */
+            /* $('.mainData').css('background', mainData_bc); */
 
         }
 
@@ -103,6 +195,7 @@ $(() => {
 
                 if(high_contrast){
                     high_contrast = false;
+                    defaultColors();
                 }else{
                     high_contrast = true;
                     $('.sidebar, .sidebar li, nav, .home-section, .toolbar-inner, #spaceTopMenu, div, button, .mainData, .row').css('background-color', 'black');
@@ -117,6 +210,7 @@ $(() => {
 
                 if(negative_contrast){
                     negative_contrast = false;
+                    defaultColors();
                 }else{
                     negative_contrast = true; 
                     $('.sidebar, .sidebar li, nav, .home-section, .toolbar-inner, #spaceTopMenu, div, button, .mainData, .row').css('background-color', 'black');
@@ -131,6 +225,7 @@ $(() => {
 
                 if(white_background){
                     white_background = false;
+                    defaultColors();
                 }else{
                     white_background = true; 
                     $('.sidebar, .sidebar li, nav, .home-section, .toolbar-inner, #spaceTopMenu, div, button, .mainData, .row').css('background-color', 'white');
